0546cc728e45999485e2653cabf96f924f60f351
Ray Lee <fsdp0dev@gmail.com>
2023-06-25 00:19:38 +0900

    V 1.0.0 : Rewrite query with named parameter

--- src/main/java/com/boardapp/boardapi/board/repository/BoardSql.java
+++ src/main/java/com/boardapp/boardapi/board/repository/BoardSql.java
@@ -9,7 +9,7 @@ public class BoardSql {
     protected final static String SELECT_BY_ID = """
             SELECT *
             FROM boards.board
-            WHERE board_id = ?
+            WHERE board_id = :board
             """;
 
     protected final static String INSERT_BOARD = """
@@ -18,20 +18,25 @@ public class BoardSql {
                 user_id,
                 board_content,
                 created_date
-            ) VALUES(?, ?, ?, ?)
+            ) VALUES(
+                :boardTitle,
+                :userId,
+                :boardContent,
+                :createdDate
+            )
             """;
 
     protected final static String UPDATE_BY_ID = """
             UPDATE boards.board
-            SET board_title = ?,
-                board_content = ?,
-                modified_date = ?
-            WHERE board_id = ?
+            SET board_title = :boardTitle,
+                board_content = :boardContent,
+                modified_date = :modifiedDate
+            WHERE board_id = :boardId
             """;
 
     protected final static String DELETE_BY_ID = """
             DELETE
             FROM boards.board
-            WHERE board_id = ?
+            WHERE board_id = :boardId
             """;
 }
