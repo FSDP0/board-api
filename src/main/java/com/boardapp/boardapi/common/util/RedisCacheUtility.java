a1cc29a956e46c8544515fb07909dcf30f3ae0b8
Ray Lee <fsdp0dev@gmail.com>
2023-07-10 16:42:48 +0900

    11

---
+++ src/main/java/com/boardapp/boardapi/common/util/RedisCacheUtility.java
@@ -0,0 +1,12 @@
+package com.boardapp.boardapi.common.util;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RedisCacheUtility {
+
+
+
+}
