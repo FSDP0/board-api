a715cb69493a67ead7f454184eb593a42c9580af
Ray Lee <fsdp0dev@gmail.com>
2023-07-13 09:47:01 +0900

    V 1.0.0 : Add comment

--- src/main/java/com/boardapp/boardapi/board/service/BoardService.java
+++ src/main/java/com/boardapp/boardapi/board/service/BoardService.java
@@ -3,7 +3,6 @@ package com.boardapp.boardapi.board.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.boardapp.boardapi.board.entity.Board;
@@ -17,27 +16,26 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class BoardService {
+    // ! Dependency injection
     private final BoardRepository boardRepository;
-
     private final RedisOperator<BoardDto> redisOperator;
 
+    // ! Cache Identification Basic Key
     private static String BOARD_KEY = "BOARD:NUM";
+    
+    // ? Cache Data는 Client측에서 필요한 경우에 Cache Hit를 통해 접근
+    // ? Cache Data의 TTL(Time to Live)은 어느정도로 설정해야 하는가?
+    // ? Cache Data가 CRUD 작업에서 Set되고, Delete되어야 하는 순간은 어느 시점인가?
+    // ! Cache Data의 Hit비율에 근거하여, Redis에 저장되어지는 시간 즉, TTL(Time to Live)의 설정시간이 달라져야 한다.
 
+    // ! List는 반드시 필수적인 경우에만 사용 권고
     public List<BoardDto> getAllBoards() {
-        List<BoardDto> dtoList = this.redisOperator.getListValue(BOARD_KEY);
+        List<BoardDto> dtoList = new ArrayList<BoardDto>();
         
-        if(dtoList == null) {
-            List<BoardDto> dtoRepoList = new ArrayList<BoardDto>();
-            
-            Iterable<Board> boardList = this.boardRepository.findAll();
-            
-            for (Board board : boardList) {
-                dtoRepoList.add(board.toDto());
-            }
-
-            redisOperator.setList(BOARD_KEY, dtoRepoList, Long.valueOf(10), TimeUnit.SECONDS);
-
-            return dtoRepoList;
+        Iterable<Board> boardList = this.boardRepository.findAll();
+        
+        for (Board board : boardList) {
+            dtoList.add(board.toDto());
         }
 
         return dtoList;
@@ -61,32 +59,37 @@ public class BoardService {
         return dto;
     }
 
+    // ! 게시글 생성 이후에, Cache Data를 삭제하여 Cache Hit의 경우에만 다시 불러올 수 있도록 구성
     @Transactional
     public void createBoard(BoardDto dto) {
         String key = BOARD_KEY + dto.getNum();
 
-        this.redisOperator.set(key,dto,Long.valueOf(10),TimeUnit.SECONDS);
-
+        // * 1. Database 삽입 작업 진행
         this.boardRepository.save(dto.toEntity());
+
+        // * 2. Cache Data 삭제
+        this.redisOperator.delete(key);
     }
 
     @Transactional
     public void editBoard(Long id, BoardDto dto) {
         String key = BOARD_KEY + id;
 
-        this.redisOperator.delete(key);
-
-        this.redisOperator.set(key, dto, Long.valueOf(10), TimeUnit.SECONDS);
-
+        // * 1. Database 갱신 작업 수행
         this.boardRepository.updateBoard(dto.toEntity(id));
+
+        // * 2. Cache Data 삭제
+        this.redisOperator.delete(key);
     }
 
     @Transactional
     public void removeBoard(Long id) {
         String key = BOARD_KEY + id;
-
-        this.redisOperator.delete(key);
-
+        
+        // * 1. Database 삭제 작업 진행
         this.boardRepository.deleteById(id);
+
+        // * 2. 존재하고 있는 Cache Data 삭제
+        this.redisOperator.delete(key);
     }
 }
